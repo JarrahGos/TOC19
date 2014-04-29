50925d19d206304142e4649ed80ec80a54433223
jarrah-95 <jarrah-95@live.com.au>
2014-04-30 00:49:20 +1000

    Removed more unnecessary code.
    Changed manner of licence comments.

--- src/TOC19/Person.java
+++ src/TOC19/Person.java
@@ -1,19 +1,21 @@
-//package TOC19;
-//    TOC19 is a simple program to run TOC payments within a small group. 
-//    Copyright (C) 2014  Jarrah Gosbell
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/***
+*    TOC19 is a simple program to run TOC payments within a small group. 
+*    Copyright (C) 2014  Jarrah Gosbell
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*
 * Author: Jarrah Gosbell
 * Student Number: z5012558
