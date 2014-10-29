7ddda3360df59034925c70e5597ddc259eb19204
jarrah-95 <jarrah-95@live.com.au>
2014-10-30 01:28:25 +1100

    Begin building SQL Functionality.

---
+++ src/TOC19/SQLInterface.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TOC19;
+
+/**
+ *
+ * @author jarrah
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+
+public class SQLInterface {
+	private String URL = "jdbc:mysql//localhost:3306/toc";
+	private String user = "jarrah";
+	private String password = "password";
+	private Connection db = null;  
+	public SQLInterface(String extuser, String extpassword) 
+	{
+		if(extuser != null) {
+			user = extuser;
+		}
+		if(extpassword != null) {
+			password = extpassword;
+		}
+		try {
+			db = DriverManager.getConnection(URL, user, password);
+		}
+		catch (java.sql.SQLException e){
+			System.out.println("error connecting to DB, check the settings" + e.toString());
+		}
+	}
+	public final String SQLread(String columnName, String table, String where) 
+	{
+		String result = "";
+		ResultSet rs = null;
+		try {
+			Statement request = db.createStatement();
+			rs = request.executeQuery("SELECT " + columnName + " FROM " + table + " WHERE " + where);
+		}
+		catch (java.sql.SQLException e) {
+			System.out.println("Unable to read database.\n" + e.toString());
+		}
+		try {
+			if(rs != null)
+			result = rs.getString(1);
+		}
+		catch (java.sql.SQLException e) {
+			System.out.println("could not read string from result" + e.toString());
+		}
+		return result;
+	}
+	public final void SQLSend(String table, String set, String where)
+	{
+		try {
+			Statement request = db.createStatement();
+			request.executeQuery("UPDATE " + table + " SET " + set + " WHERE " + where);
+		}
+		catch (java.sql.SQLException e) {
+			System.out.println("Unable to write to database.\n" + e.toString());
+		}
+	}
+}
